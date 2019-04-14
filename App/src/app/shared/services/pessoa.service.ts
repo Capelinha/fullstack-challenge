5f136968b9e005b25b2ba5128e24c514e77bc31b
Capelinha <mateus@nuplex.com.br>
2019-04-14 17:12:35 -0300

    Metodos criados e conectados com a API

--- App/src/app/shared/services/pessoa.service.ts
+++ App/src/app/shared/services/pessoa.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+import {HttpClient} from "@angular/common/http";
+import {PessoaModel} from "../models/pessoa.model";
+import {Observable} from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
 
-  constructor() { }
+  private readonly endpoint: string = "https://k92gnvpow4.execute-api.us-east-2.amazonaws.com/beta/pessoa";
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<PessoaModel[]>{
+    return this.http.get<PessoaModel[]>(this.endpoint);
+  }
+
+  save(pessoa: PessoaModel){
+    this.http.post(this.endpoint, JSON.stringify(pessoa));
+  }
 }
