00edd15530bf9b4f76027a35de5b0db088a9fcaf
Capelinha <mateus@nuplex.com.br>
2019-04-14 16:41:55 -0300

    Criação do grafico

--- App/src/app/lista-pessoas/lista-pessoas.component.ts
+++ App/src/app/lista-pessoas/lista-pessoas.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {PessoaModel} from "../shared/models/pessoa.model";
+
+import {Chart} from 'chart.js';
+import {delay} from "q";
 
 @Component({
   selector: 'lista-pessoas',
@@ -8,12 +11,70 @@ import {PessoaModel} from "../shared/models/pessoa.model";
 })
 export class ListaPessoasComponent implements OnInit {
 
-  //pessoas: PessoaModel[] = [{id : "hhnOHFuhwhifhiIHFAW", nome : "Mateus", sobrenome : "Igreja", participacao : 10}];
-  pessoas: PessoaModel[];
-  
+  //@ViewChild('grafico')
+  elGrafico: ElementRef;
+  grafico: Chart;
+  pessoas: PessoaModel[] = [{id : "hhnOHFuhwhifhiIHFAW", nome : "Mateus", sobrenome : "Igreja", participacao : 10}];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //Disparar criação do grafico assim que o canvas for exibido na tela
+  @ViewChild('grafico')
+  set elementoGrafico(el: ElementRef) {
+    this.elGrafico = el;
+    this.criarGrafico();
+  };
+
+  criarGrafico() {
+    const ctx = this.elGrafico.nativeElement.getContext('2d');
+
+    const dados = {
+      datasets: [{
+        data : this.pessoas.map((pessoa) => pessoa.participacao),
+        backgroundColor: this.pessoas.map( () => this.getRandomColor())
+      }],
+      labels: this.pessoas.map((pessoa) => `${pessoa.nome} ${pessoa.sobrenome}`),
+      type: 'pie'
+    };
+
+    const opcoes = {
+      aspectRatio : 1.7,
+      legend: {
+        position: 'right',
+        labels: {
+          fontSize : 14,
+          fontStyle : 'bold',
+          boxWidth : 14
+        }
+      },
+      layout: {
+        padding: {
+          left: 5,
+          right: 5,
+          top: 10,
+          bottom: 10
+        }
+      }
+    };
+
+    this.grafico = new Chart(ctx, {
+      type: 'doughnut',
+      data: dados,
+      options: opcoes
+    });
+
+  }
+
+  getRandomColor() {
+    const letters = '456789ABCDEF'.split('');
+    let color = '#';
+    for (let i = 0; i < 6; i++ ) {
+      color += letters[Math.round(Math.random() * (letters.length - 1))];
+    }
+    return color;
   }
 
 }
