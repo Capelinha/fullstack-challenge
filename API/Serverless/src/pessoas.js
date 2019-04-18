6296def4954887d643fcc6ce64e9124d58944692
unknown <mateus@nuplex.com.br>
2019-04-18 14:53:40 -0300

    Inicio das validações com Structure

---
+++ API/Serverless/src/pessoas.js
@@ -0,0 +1,29 @@
+const { attributes } = require('structure');
+
+const Pessoa = attributes({
+  nome: {
+    type: String,
+    minLength: 1,
+    required: true
+  },
+  sobrenome: {
+    type: String,
+    minLength: 1,
+    required: true
+  },
+  participacao: {
+    type: Number,
+    integer: true,
+    required: true
+  }
+})(class Pessoa { });
+
+// const pessoa = new Pessoa({
+//   nome: 'Mateus',
+//   sobrenome: 'Igreja',
+//   participacao: 10
+// });
+//
+// const { valid, errors } = pessoa.validate();
+
+module.exports = Pessoa;
